f9771efca833c9cdb24077a9d7b53d6fc4b52923
Alexey Kosov <kasaff322@icloud.com>
2023-08-17 17:00:59 +0300

    add: DetailsView

--- RickAndMortyTestTask/DetailsView.swift
+++ RickAndMortyTestTask/DetailsView.swift
@@ -26,31 +26,71 @@ struct DetailsView: View {
                         .foregroundColor(.gray)
                 }
             }
+            Section {
+                VStack {
+                    HStack {
+                        Text("Species:")
+                        Spacer()
+                        Text(character.species)
+                    }
 
-            VStack {
-                HStack {
-                    Text("Species:")
-                    Spacer()
-                    Text(character.species)
+                    HStack {
+                        Text("Type:")
+                        Spacer()
+                        Text(!character.type.isEmpty ? character.type : "Unknown")
+                    }
+                    .padding(.vertical)
+
+                    HStack {
+                        Text("Gender: ")
+                        Spacer()
+                        Text(character.gender)
+                    }
                 }
-
+                .padding()
+                .foregroundColor(.white)
+                .background(Color(red: 0.15, green: 0.16, blue: 0.22))
+                .clipShape(RoundedRectangle(cornerRadius: 16))
+            } header: {
                 HStack {
-                    Text("Type:")
+                    Text("Info")
+                        .font(.headline)
+                        .foregroundColor(.white)
                     Spacer()
-                    Text(!character.type.isEmpty ? character.type : "Unknown")
-                }
-                .padding(.vertical)
-
-                HStack {
-                    Text("Gender: ")
-                    Spacer()
-                    Text(character.gender)
                 }
             }
-            .foregroundColor(.white)
-            .padding()
-            .background(Color(red: 0.15, green: 0.16, blue: 0.22))
-            .clipShape(RoundedRectangle(cornerRadius: 16))
+
+            Section {
+                HStack {
+                    ZStack {
+                        Rectangle()
+                            .frame(width: 64, height: 64)
+                            .background(Color(red: 0.1, green: 0.11, blue: 0.16))
+                            .cornerRadius(10)
+
+                        Image("planet")
+                    }
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text(character.origin.name)
+                        Text("Planet")
+                            .foregroundColor(.green)
+                    }
+                    .foregroundColor(.white)
+
+                    Spacer()
+                }
+                .padding()
+                .background(Color(red: 0.15, green: 0.16, blue: 0.22))
+                .clipShape(RoundedRectangle(cornerRadius: 16))
+            } header: {
+                HStack {
+                    Text("Origin")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                    Spacer()
+                }
+            }
+            
         }
         .padding()
     }
